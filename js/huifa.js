dc404798c6457db8caf246a8d171bd2edfc1129a
jkkwjk <465640448@qq.com>
2018-12-13 12:45:33 +0800

    index完成

---
+++ js/huifa.js
@@ -0,0 +1,27 @@
+function post(label){
+	var arr = [];
+	
+	var id=document.getElementById('text2');
+	var text=id.value;
+	if(label==0)
+	{
+		arr.push("您发帖的内容是:");
+	}
+	else
+	{
+		arr.push("您回帖的内容是:");
+	}
+	arr.push(text);
+	alert(arr.join("\n"));
+	window.open("","_self").close();
+}
+function huifa(url,text)
+{
+	document.cookie=text;
+	window.open(url);
+}
+function load()
+{
+	var text=document.cookie;
+	document.getElementById('ttext').innerHTML = text;
+}
